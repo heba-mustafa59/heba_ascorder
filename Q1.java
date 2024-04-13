f48146326eaeb7576cc6eb44c0aced97c2aaadb5
Heba_Mustafa🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-04-13 12:59:14 +0300

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,28 @@
+package q1;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q1 {//heba jalil
+
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        int n ;
+        n= in.nextInt();
+      
+        for (int i = 0; i < n; i++) {
+           
+        }
+      
+       
+    }
+     public static void prime(int number){
+         
+         
+     }
+        
+              
+              
+              
+        
+}
+
